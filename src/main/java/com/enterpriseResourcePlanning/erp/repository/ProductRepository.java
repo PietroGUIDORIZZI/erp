3163048ce682bae35b09148957b26d33870232fc
PietroBin <Peterguidorizzi@gmail.com>
2026-06-27 21:38:24 -0300

    feat: ProductRepository

---
+++ src/main/java/com/enterpriseResourcePlanning/erp/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.enterpriseResourcePlanning.erp.repository;
+
+import com.enterpriseResourcePlanning.erp.entity.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
